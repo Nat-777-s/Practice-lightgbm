26f715bc276f5b5c7553072288323b4b96da7582
Nat-sat <54437061+Nat-777-s@users.noreply.github.com>
2024-01-25 16:03:38 +0900

    Import iris data set

--- pr-1.py
+++ pr-1.py
@@ -14,5 +14,19 @@ iris = load_iris()
 df_iris = pd.DataFrame(iris.data, columns=iris.feature_names)
 df_iris['target'] = iris.target
 
-print(df_iris.shape)
+# print(df_iris.shape) #irisデータセットのサイズを確認
 df_iris.head()
+
+# #データをプロットする
+# fig = plt.figure(figsize = (5,5))
+# ax = fig.add_subplot(111)
+
+# #各ラベルごとに花弁の長さをx軸、花弁の幅をy軸に描画
+# plt.scatter(df_iris[df_iris['target']==0].iloc[:,0], df_iris[df_iris['target']==0].iloc[:,1], label='Setosa')
+# plt.scatter(df_iris[df_iris['target']==1].iloc[:,0], df_iris[df_iris['target']==1].iloc[:,1], label='Versicolour')
+# plt.scatter(df_iris[df_iris['target']==2].iloc[:,0], df_iris[df_iris['target']==2].iloc[:,1], label='Virginica')
+# plt.xlabel("sepal length[cm]", fontsize=13)
+# plt.ylabel("sepal width[cm]", fontsize=13)
+
+# plt.legend()
+# plt.show()
