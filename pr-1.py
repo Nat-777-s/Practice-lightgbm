da24d63e833acbf08fb6ba6852b820849a75d229
Nat-sat <54437061+Nat-777-s@users.noreply.github.com>
2024-01-25 15:54:06 +0900

    Import iris data set

--- pr-1.py
+++ pr-1.py
@@ -2,10 +2,17 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-print(pd.__version__) #スクリプト上でバージョンを確認できる。
+
+# print(pd.__version__) #スクリプト上でバージョンを確認できる。
 
 from sklearn.datasets import load_iris
 
 iris = load_iris()
 #print(iris.DESCR)  #データセットに関する説明を表示
-print(type(iris))
+# print(type(iris)) #インポートできているか確認
+
+df_iris = pd.DataFrame(iris.data, columns=iris.feature_names)
+df_iris['target'] = iris.target
+
+print(df_iris.shape)
+df_iris.head()
